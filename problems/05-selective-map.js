70ceccffdbe77897877645e38df3514fff314e0c
Matthew Pierik <pierikm@umich.edu>
2021-10-27 14:43:19 -0400

    Solved problem 5

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -42,11 +42,41 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 - How many times are you calling each callback function?
 *******************************************************************************/
 
-let selectiveMap = function() {
-
+let selectiveMap = function(arr, cb1, cb2) {
+    for(let i = 0; i < arr.length; i++){
+        if(cb1(arr[i])){
+            if(cb2(arr[i])){
+                arr[i] = cb2(arr[i]);
+            }
+        }
+    }
+    return arr;
 };
 
+function isEven(n) {
+    return n % 2 === 0;
+}
 
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
 
 
 
