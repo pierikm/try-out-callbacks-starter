f4a8e64b5a247bbbe95f01d0d52d7ee659a4886e
Matthew Pierik <pierikm@umich.edu>
2021-10-27 15:52:26 -0400

    Solved problem 15

--- problems/15-sentence-mapper.js
+++ problems/15-sentence-mapper.js
@@ -25,8 +25,12 @@ let result2 = sentenceMapper("this is pretty cool right", removeVowels);
 console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
-let sentenceMapper = function() {
+let sentenceMapper = function(sentence, cb) {
+    sentence = sentence.split(' ');
 
+    sentence = sentence.map(ele => cb(ele));
+
+    return sentence.join(' ');
 };
 
 
