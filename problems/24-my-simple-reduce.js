d734483d3dc1477d5daca0b664e5955736ff561c
Matthew Pierik <pierikm@umich.edu>
2021-10-27 16:57:14 -0400

    Solved problem 24

--- problems/24-my-simple-reduce.js
+++ problems/24-my-simple-reduce.js
@@ -34,8 +34,13 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   optional initial accumulator
 *******************************************************************************/
 
-let mySimpleReduce = function() {
+let mySimpleReduce = function(arr, cb) {
+    let value = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        value = cb(value, arr[i]);
+    }
 
+    return value;
 };
 
 
