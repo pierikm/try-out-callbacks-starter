f58b7473a2b42424b00c26ef7dc4b10a3c044bc6
Matthew Pierik <pierikm@umich.edu>
2021-10-27 16:45:16 -0400

    Solved problem 22

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -22,12 +22,9 @@ let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
 console.log(result3); // -1
 *******************************************************************************/
 
-let firstIndex = function() {
-
+let firstIndex = function(arr, cb) {
+    return arr.map(ele => cb(ele)).indexOf(true);
 };
-
-
-
 
 
 
