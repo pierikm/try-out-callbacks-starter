4ead79d6fbbece24178061c923130ded568a4c54
Offutt <broffutt@gmail.com>
2021-10-27 13:27:49 -0500

    Problem 1 complete

--- problems/01-my-for-each.js
+++ problems/01-my-for-each.js
@@ -21,9 +21,11 @@ myForEach(['laika', 'belka'], function (el) {
 console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
 
-let myForEach = function() {
-
-};
+function myForEach(arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+        cb(arr[i], i, arr);
+    }
+}
 
 
 
