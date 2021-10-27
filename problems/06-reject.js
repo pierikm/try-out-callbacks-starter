f13a89a882aa2d0c2a35e74011727ff32dcc7c09
Matthew Pierik <pierikm@umich.edu>
2021-10-27 14:48:48 -0400

    Solved problem 6

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -21,11 +21,23 @@ let hasA = function(s) {
 console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 *******************************************************************************/
 
-let reject = function() {
-
+let reject = function(arr, cb) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(!cb(arr[i])) newArr.push(arr[i]);
+    }
+    return newArr;
 };
 
+let isEven = function(n) {
+    return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
 
+let hasA = function(s) {
+    return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 
 
 
